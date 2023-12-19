cb0eab403b1001912a8c876a6a1bc956095158bc
KattanMohammad <mohammadsy107@gmail.com>
2023-12-19 15:37:57 +0100

    corriger la méthode envelppeConvexe

--- src/algorithms/DefaultTeam.java
+++ src/algorithms/DefaultTeam.java
@@ -16,13 +16,8 @@ public class DefaultTeam {
     if (points.size()<3) {
       return null;
     }
-
     Point p=points.get(0);
     Point q=points.get(1);
-
-    /*******************
-     * PARTIE A ECRIRE *
-     *******************/
     double maxDistance = distance(p, q);
     for (int i = 0; i < points.size(); i++) {
       for (int j = i + 1; j < points.size(); j++) {
@@ -61,61 +56,43 @@ public class DefaultTeam {
     if (points.isEmpty()) {
       return null;
     }
-
     Point center=points.get(0);
     int radius=100;
-
-    /*******************
-     * PARTIE A ECRIRE *
-     *******************/
     return new Circle(center,radius);
   }
-  /
-
-  // enveloppeConvexe: ArrayList<Point> --> ArrayList<Point>
   //   renvoie l'enveloppe convexe de la liste.
   public ArrayList<Point> enveloppeConvexe(ArrayList<Point> points){
-    if (points.size()<4) {
-      return points;
+    if (points.size()<3) {
+      return null;
     }
-
-    ArrayList<Point> enveloppe = new ArrayList<Point>();
-
-    for (Point p: points){
-      for (Point q: points){
-        if(p.equals(q)) continue;
-        Point ref = p;
-        for (Point r: points) if (crossProduct(p,q,p,r)!=0) {ref = r; break;}
-        if (ref.equals(p)) {enveloppe.add(p); enveloppe.add(q); continue;}
-        double signeRef = crossProduct(p,q,p,ref);
-        //new Line(p,q);
-        boolean estCote = true;
-
-        for (Point r: points) if(signeRef * crossProduct(p,q,p,r)<0) {estCote = false; break;}
-        // sans le break le temps de calcul sera enorme
-        if (estCote) {enveloppe.add(p); enveloppe.add(q) ;}
-
-        return enveloppe; //ici l'enveloppe n'est pas trie dans le sens trigonometrique, et contient
-
-      }
-
-    }
-
-    enveloppe.add(points.get(0));
-    enveloppe.add(points.get(1));
-    enveloppe.add(points.get(2));
-
-
-    /*******************
-     * PARTIE A ECRIRE *
-     *******************/
-    return points;
+    return exercice1(points);
   }
+
   private double crossProduct(Point p, Point q, Point s, Point t){
     return ((q.x-p.x)*(t.y-s.y)-(q.y-p.y)*(t.x-s.x));
   }
   private double distance (Point p, Point q){
     return Math.sqrt(Math.pow(p.getX() - q.getX(), 2) + Math.pow(p.getY() - q.getY(), 2));
+  }
+  private ArrayList<Point> exercice1(ArrayList<Point> points){
+    if (points.size()<4) return points;
+
+    ArrayList<Point> enveloppe = new ArrayList<Point>();
+
+    for (Point p: points) {
+      for (Point q: points) {
+        if (p.equals(q)) continue;
+        Point ref=p;
+        for (Point r: points) if (crossProduct(p,q,p,r)!=0) {ref=r;break;}
+        if (ref.equals(p)) {enveloppe.add(p); enveloppe.add(q); continue;}
+        double signeRef = crossProduct(p,q,p,ref);
+        boolean estCote = true;
+        for (Point r: points) if (signeRef * crossProduct(p,q,p,r)<0) {estCote = false;break;} //ici sans le break le temps de calcul devient horrible
+        if (estCote) {enveloppe.add(p); enveloppe.add(q);}
+      }
+    }
+
+    return enveloppe; //ici l'enveloppe n'est pas trie dans le sens trigonometrique, et contient des doublons, mais tant pis!
   }
 
 }
